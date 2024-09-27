f87d02822e60bb09b8c0796ec8bebd3e2682e3ee
Ed Copeland <edcopeland79@googlemail.com>
2024-09-27 17:09:12 +0100

    beautiful soup airport list exercise complete

---
+++ buSoupAirportList.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports()' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+
+Refer to the 'options.html' file in the tab above for a stripped down version
+of what is actually on the website. The test() assertions are based on the
+given file.
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        soup = BeautifulSoup(html, "html.parser")
+
+        airports = soup.find(attrs={"name":'AirportList'})
+
+        
+        for option in airports.find_all('option'):
+            if option['value'][:3] != 'All':
+               data.append(option['value'])        
+    print(data)
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
